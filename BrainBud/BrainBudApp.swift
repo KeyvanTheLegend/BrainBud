a721f3c79adfae9e1a84c704f279a81eaf1db2a4
KeyvanTheLegend <kayvan.yaghoubian@gmail.com>
2021-08-06 18:28:18 +0430

    init project

---
+++ BrainBud/BrainBudApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainBudApp.swift
+//  BrainBud
+//
+//  Created by sh on 8/6/21.
+//
+
+import SwiftUI
+
+@main
+struct BrainBudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
